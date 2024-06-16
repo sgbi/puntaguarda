8482b78d7ae372d7533f9d7cb95b3c0b73854d33
sgbi <83344965+sgbi@users.noreply.github.com>
2024-06-16 20:14:50 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,575 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-punta-guarda-1",
+      "name": "Punta Guarda 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.14128855849470057,
+        "pitch": 0.5622151973319944,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2115771518675853,
+          "pitch": 0.5611017276014536,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.6119577905050644,
+          "pitch": 0.5268571560861037,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-punta-guarda-2",
+      "name": "Punta Guarda 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8601988657457795,
+        "pitch": 0.5169361210368351,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.7857327191031072,
+          "pitch": 0.4767754663795376,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-punta-guarda-3",
+      "name": "Punta Guarda 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.525339690908897,
+        "pitch": 0.9199096050514619,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.8557874410796238,
+          "pitch": 0.5909686991913947,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -2.1871830909171948,
+          "pitch": 0.937975289377837,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -0.7912067243860825,
+          "pitch": 1.061554928132736,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-punta-guarda-4",
+      "name": "Punta Guarda 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8703825392186904,
+        "pitch": 0.971613512167746,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.5694814195039486,
+          "pitch": 0.7855357560518748,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 1.993375216294253,
+          "pitch": 1.2788454015748272,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-punta-guarda-5",
+      "name": "Punta Guarda 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.13297746681863387,
+        "pitch": 0.5527820564371915,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2902715245034635,
+          "pitch": 0.5449667837220886,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.6964906673940323,
+          "pitch": 0.4162942017657656,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5-punta-guarda-6",
+      "name": "Punta Guarda 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7685513927620811,
+        "pitch": 1.1261172411791982,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.2682629985450813,
+          "pitch": 0.7499058094837228,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.10070217340639509,
+          "pitch": 1.1780651102262958,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6-punta-guarda-7",
+      "name": "Punta Guarda 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9747149625694078,
+        "pitch": 0.255682257179652,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.6454908569312252,
+          "pitch": 0.6171170326595092,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -1.865344309777969,
+          "pitch": 0.4026854546887044,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -1.209654158007254,
+          "pitch": 0.48018770760232243,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "7-punta-guarda-8",
+      "name": "Punta Guarda 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5638842681329486,
+        "pitch": 0.6634335174826251,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.214425600506324,
+          "pitch": 0.5608454005706704,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.04116765704992709,
+          "pitch": 0.8638800831082705,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 2.5628812505241463,
+          "pitch": 0.802127309157882,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.9912393711519627,
+          "pitch": 0.7765495880780868,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "8-punta-guarda-9",
+      "name": "Punta Guarda 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.12466637514244638,
+        "pitch": 0.5169361210368209,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.927977732594929,
+          "pitch": 0.2741359402551069,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -0.11025800671894359,
+          "pitch": 0.691137338253446,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.031594217513745804,
+          "pitch": 0.24548095468072084,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "9-punta-guarda-10",
+      "name": "Punta Guarda 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.0910620640800754,
+        "pitch": 0.1548910073077323,
+        "fov": 1.4300641596566168
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.8600307470567667,
+          "pitch": -0.0642233533593739,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": -2.7004649716513516,
+          "pitch": 0.13192977730806277,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 3.0433746022176056,
+          "pitch": 0.4339135527988667,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "10-punta-guarda-11",
+      "name": "Punta Guarda 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.4030929743429681,
+          "pitch": 0.22604233648425875,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.13051769617940323,
+          "pitch": 0.23744852003681594,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "11-punta-guarda-12",
+      "name": "Punta Guarda 12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.15331589801128231,
+          "pitch": -0.21420709245751368,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.5977275046191579,
+          "pitch": -0.00022673647151272291,
+          "title": "Subject Property",
+          "text": ""
+        },
+        {
+          "yaw": 0.18053043828909665,
+          "pitch": 0.16589996772234095,
+          "title": "Subject Property",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Punta Guarda, Dulangan",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
